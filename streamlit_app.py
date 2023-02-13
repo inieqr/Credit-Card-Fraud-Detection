c757ef5f6cdfa612bbc0ca6b9cd26f10f445af72
inieqr <70246193+inieqr@users.noreply.github.com>
2023-02-13 12:46:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ Credit card fraud is a form of identity theft that involves an unauthorized taki
 
 **This Streamlit App utilizes a Machine Learning model served as an API in order to detect fraudulent credit card transactions based on the following criteria: hours, type of transaction, amount, balance before and after transaction etc.** 
 
-The API was built with FastAPI and can be found [here.](https://myapi-production-f978.up.railway.app//)
+The API was built with FastAPI and can be found [here.](https://myapi-production-f978.up.railway.app/)
 
 """)
 
